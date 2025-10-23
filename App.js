1fd649a67dbddd499d2fbc2d59e40902730b6199
Nika <nikadvalishvili517@gmail.com>
2025-10-24 03:27:00 +0400

    practicing the basics

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={() => console.log("image tapped")}>
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 300,
+          }}
+        />
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
   },
 });
